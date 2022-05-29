d2561d49c3fed0fd6429dc26fee2cc874a3f6e6d
Povolotskii Artem <artpovol@gmail.com>
2022-05-29 03:28:23 +0300

    add sort buttons

--- script.js
+++ script.js
@@ -1,44 +1,79 @@
-let skills = [
-    {
-        "name": "HTML",
-        "cssname": "html",
-        "value": 30,
+const skills = {
+    skillBars: [
+        {
+            name: "HTML",
+            cssname: "html",
+            value: 30,
+        },
+        {
+            name: "CSS",
+            cssname: "css",
+            value: 10,
+        },
+        {
+            name: "C++",
+            cssname: "cpp",
+            value: 40,
+        },
+        {
+            name: ".NET",
+            cssname: "net",
+            value: 50,
+        },
+        {
+            name: "SQL",
+            cssname: "sql",
+            value: 30,
+        },
+        {
+            name: "Python",
+            cssname: "py",
+            value: 40,
+        }
+    ],
+    topSorted: true,
+    drawSkillBars() {
+        const dl = document.createElement("dl");
+        dl.classList.add("skills-list");
+        document.querySelector('.skills').append(dl);
+
+        this.skillBars.forEach(element => {
+            const newSkill = document.createElement("dt");
+            newSkill.classList.add(`skill-${element.cssname}`);
+            newSkill.textContent = element.name;
+            
+            const newSkillBar = document.createElement("dd");
+            newSkillBar.classList.add("level");
+            newSkillBar.textContent = `${element.value}%`;
+            
+            const newDiv = document.createElement("div");
+            newDiv.style.width = `${element.value}%`;
+            newSkillBar.append(newDiv);
+    
+            dl.append(newSkill, newSkillBar)
+        });
     },
-    {
-        "name": "CSS",
-        "cssname": "css",
-        "value": 10,
+    nameSort(a, b) {
+        let x = a.name;
+        let y = b.name;
+        return x < y ? -1 : x > y ? 1 : 0;
     },
-    {
-        "name": "C++",
-        "cssname": "cpp",
-        "value": 40,
+    valueSort(a, b) {
+        return a.value - b.value;
     },
-    {
-        "name": ".NET",
-        "cssname": "net",
-        "value": 50,
-    },
-    {
-        "name": "SQL",
-        "cssname": "sql",
-        "value": 30,
+}
+
+document.addEventListener('click', function(event) {
+    if (event.target.closest(".sort-button")) {
+        document.querySelector(".skills-list").remove();
+        skills.skillBars.sort(
+            !event.target.closest(".sort-value") ?
+            skills.nameSort : skills.valueSort
+        );
+        if (skills.topSorted = !skills.topSorted) 
+        skills.skillBars.reverse();
+        skills.drawSkillBars();
     }
-]
-
-const placeIn = document.querySelector(".skills-list");
-skills.forEach(element => {
-    const newSkill = document.createElement("dt");
-    newSkill.classList.add(`skill-${element.cssname}`);
-    newSkill.textContent = element.name;
-    
-    const newSkillBar = document.createElement("dd");
-    newSkillBar.classList.add("level");
-    newSkillBar.textContent = `${element.value}%`;
-    
-    const newDiv = document.createElement("div");
-    newDiv.style = `width: ${element.value}%`;
-    newSkillBar.append(newDiv);
-
-    placeIn.append(newSkill, newSkillBar)
 });
+
+skills.drawSkillBars();
