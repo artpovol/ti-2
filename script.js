465fa91c9b6285a9b4e5b1f200000708869670b9
Povolotskii Artem <artpovol@gmail.com>
2022-06-19 21:04:26 +0300

    added skills.json database

--- script.js
+++ script.js
@@ -1,77 +1,54 @@
-const skills = {
-    skillBars: [
-        {
-            name: "HTML",
-            cssname: "html",
-            value: 30,
-        },
-        {
-            name: "CSS",
-            cssname: "css",
-            value: 10,
-        },
-        {
-            name: "C++",
-            cssname: "cpp",
-            value: 40,
-        },
-        {
-            name: ".NET",
-            cssname: "net",
-            value: 50,
-        },
-        {
-            name: "SQL",
-            cssname: "sql",
-            value: 30,
-        },
-        {
-            name: "Python",
-            cssname: "py",
-            value: 40,
+'use strict'
+
+fetch('db/skills.json')
+    .then(data => data.json())
+    .then(json => {
+        const skills = {
+            skillBars: json,
+            topSorted: true,
+            drawSkillBars() {
+                const dl = document.createElement("dl");
+                dl.classList.add("skills-list");
+                document.querySelector('.skills').append(dl);
+                
+                this.skillBars.forEach(element => {
+                    const newSkill = document.createElement("dt");
+                    newSkill.classList.add(`skill-${element.cssname}`);
+                    newSkill.textContent = element.name;
+                    
+                    const newSkillBar = document.createElement("dd");
+                    newSkillBar.classList.add("level");
+                    newSkillBar.textContent = `${element.value}%`;
+                    
+                    const newDiv = document.createElement("div");
+                    newDiv.style.width = `${element.value}%`;
+                    newSkillBar.append(newDiv);
+                    
+                    dl.append(newSkill, newSkillBar)
+                });
+            },
+            nameSort(a, b) {
+                let x = a.name;
+                let y = b.name;
+                return x < y ? -1 : x > y ? 1 : 0;
+            },
+            valueSort(a, b) {
+                return a.value - b.value;
+            },
         }
-    ],
-    topSorted: true,
-    drawSkillBars() {
-        const dl = document.createElement("dl");
-        dl.classList.add("skills-list");
-        document.querySelector('.skills').append(dl);
-
-        this.skillBars.forEach(element => {
-            const newSkill = document.createElement("dt");
-            newSkill.classList.add(`skill-${element.cssname}`);
-            newSkill.textContent = element.name;
-            
-            const newSkillBar = document.createElement("dd");
-            newSkillBar.classList.add("level");
-            newSkillBar.textContent = `${element.value}%`;
-            
-            const newDiv = document.createElement("div");
-            newDiv.style.width = `${element.value}%`;
-            newSkillBar.append(newDiv);
-    
-            dl.append(newSkill, newSkillBar)
+        
+        document.querySelector(".sort-buttons").addEventListener('click', function(event) {
+                document.querySelector(".skills-list").remove();
+                skills.skillBars.sort(
+                    event.target.closest(".sort-name") ?
+                    skills.nameSort : skills.valueSort
+                );
+                if (skills.topSorted = !skills.topSorted) 
+                    skills.skillBars.reverse();
+                skills.drawSkillBars();
         });
-    },
-    nameSort(a, b) {
-        let x = a.name;
-        let y = b.name;
-        return x < y ? -1 : x > y ? 1 : 0;
-    },
-    valueSort(a, b) {
-        return a.value - b.value;
-    },
-}
-
-document.querySelector(".sort-buttons").addEventListener('click', function(event) {
-        document.querySelector(".skills-list").remove();
-        skills.skillBars.sort(
-            event.target.closest(".sort-name") ?
-            skills.nameSort : skills.valueSort
-        );
-        if (skills.topSorted = !skills.topSorted) 
-        skills.skillBars.reverse();
+        
         skills.drawSkillBars();
-});
-
-skills.drawSkillBars();
+        
+    })
+    .catch(() => console.error("не удалось загрузить список навыков"));
